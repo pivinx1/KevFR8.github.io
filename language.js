6181104d5fe8cc8e201fd29d5120a302bceb7537
zai-tm <tusharmistry444@gmail.com>
2021-08-16 13:10:50 -0600

    should be the last fix

--- language.js
+++ language.js
@@ -8,7 +8,6 @@ if (document.cookie.split(';').some((item) => item.includes('lang=en'))) {
     document.getElementById("contact").innerHTML = "Contact me";
     document.getElementById("computer_fan").innerHTML = "Are you a fan of computers? This site lets you download my packs and much more!<br/> Don't hesitate to contact me in case of problems!";
     document.getElementById("current_video").innerHTML = "Latest video";
-    document.getElementById("build").innerHTML = "Build 99 Translation Update";
     document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Translations done by Zai";
 } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
     document.getElementById("welcome").innerHTML = "Bienvenue sur FireWave !";
@@ -17,7 +16,6 @@ if (document.cookie.split(';').some((item) => item.includes('lang=en'))) {
     document.getElementById("contact").innerHTML = "Me Contactez";
     document.getElementById("computer_fan").innerHTML = "Fan de l'informatique ? Ce site vous permet de découvrir mes packs et bien plus encore !<br/>N'hésitez pas à me contactez en cas de problèmes !";
     document.getElementById("current_video").innerHTML = "La vidéo du moment !";
-    document.getElementById("build").innerHTML = "Build 99 Traduction Update";
     document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Traduction réalisée par Zai";
 }
 }
@@ -31,7 +29,6 @@ function langSwitch() {
         document.getElementById("contact").innerHTML = "Contact me";
         document.getElementById("computer_fan").innerHTML = "Are you a fan of computers? This site lets you download my packs and much more!<br/> Don't hesitate to contact me in case of problems!";
         document.getElementById("current_video").innerHTML = "Latest video";
-        document.getElementById("build").innerHTML = "Build 99 Translation Update";
         document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Translations done by Zai";
     } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
         document.getElementById("welcome").innerHTML = "Bienvenue sur FireWave !";
@@ -40,7 +37,6 @@ function langSwitch() {
         document.getElementById("contact").innerHTML = "Me Contactez";
         document.getElementById("computer_fan").innerHTML = "Fan de l'informatique ? Ce site vous permet de découvrir mes packs et bien plus encore !<br/>N'hésitez pas à me contactez en cas de problèmes !";
         document.getElementById("current_video").innerHTML = "La vidéo du moment !";
-        document.getElementById("build").innerHTML = "Build 99 Traduction Update";
         document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Traduction réalisée par Zai";
     }
 }
