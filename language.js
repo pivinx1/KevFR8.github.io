4e127122d41d03ea5b9121b2b484c1350179bc99
KevFR <70813133+KevFR8@users.noreply.github.com>
2021-08-16 07:52:54 +0200

    Summary

---
+++ language.js
@@ -0,0 +1,46 @@
+function cookieLang() {
+    console.log(document.cookie)
+    if (document.cookie.split(';').some((item) => item.includes('lang=en'))) {
+        document.getElementById("language").value = "en";
+        document.getElementById("welcome").innerHTML = "Welcome to FireWave!";
+        document.getElementById("windows7pack").innerHTML = "Windows 7 pack";
+        document.getElementById("about").innerHTML = "About me";
+        document.getElementById("contact").innerHTML = "Contact me";
+        document.getElementById("computer_fan").innerHTML = "Are you a fan of computers? This site lets you download my packs and much more!<br/> Don't hesitate to contact me in case of problems!";
+        document.getElementById("current_video").innerHTML = "Latest video";
+        document.getElementById("build").innerHTML = "Build 99 Translation Update (part 1)";
+        document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Translations done by Zai";
+    } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
+        document.getElementById("welcome").innerHTML = "Bienvenue sur FireWave !";
+        document.getElementById("windows7pack").innerHTML = "Pack Windows 7";
+        document.getElementById("about").innerHTML = "A propois de moi";
+        document.getElementById("contact").innerHTML = "Me Contactez";
+        document.getElementById("computer_fan").innerHTML = "Fan de l'informatique ? Ce site vous permet de découvrir mes packs et bien plus encore !<br/>N'hésitez pas à me contactez en cas de problèmes !";
+        document.getElementById("current_video").innerHTML = "La vidéo du moment !";
+        document.getElementById("build").innerHTML = "Build 99 Traduction Update (partie 1)";
+        document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Traduction réalisée par Zai";
+    }
+    }
+    function langSwitch() {
+        var langId = document.getElementById("language").value;
+        document.cookie = `lang=${langId};`
+        if (document.cookie.split(';').some((item) => item.includes('lang=en'))) {
+            document.getElementById("welcome").innerHTML = "Welcome to FireWave!";
+            document.getElementById("windows7pack").innerHTML = "Windows 7 pack";
+            document.getElementById("about").innerHTML = "About me";
+            document.getElementById("contact").innerHTML = "Contact me";
+            document.getElementById("computer_fan").innerHTML = "Are you a fan of computers? This site lets you download my packs and much more!<br/> Don't hesitate to contact me in case of problems!";
+            document.getElementById("current_video").innerHTML = "Latest video";
+            document.getElementById("build").innerHTML = "Build 99 Translation Update (part 1)";
+            document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Translations done by Zai";
+        } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
+            document.getElementById("welcome").innerHTML = "Bienvenue sur FireWave !";
+            document.getElementById("windows7pack").innerHTML = "Pack Windows 7";
+            document.getElementById("about").innerHTML = "A propois de moi";
+            document.getElementById("contact").innerHTML = "Me Contactez";
+            document.getElementById("computer_fan").innerHTML = "Fan de l'informatique ? Ce site vous permet de découvrir mes packs et bien plus encore !<br/>N'hésitez pas à me contactez en cas de problèmes !";
+            document.getElementById("current_video").innerHTML = "La vidéo du moment !";
+            document.getElementById("build").innerHTML = "Build 99 Traduction Update (partie 1)";
+            document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Traduction réalisée par Zai";
+        }
+    }
---
+++ language5.js
@@ -0,0 +1,70 @@
+function cookieLang() {
+    console.log(document.cookie)
+    if (document.cookie.split(';').some((item) => item.includes('lang=en'))) {
+        document.getElementById("language").value = "en";
+        document.getElementById("correction").innerHTML = "Fixed a problem with the background when zooming out!";
+        document.getElementById("javascript").innerHTML = "Javascript test";
+        document.getElementById("changelog").innerHTML = "Added the changelog";
+        document.getElementById("kevcoorp").innerHTML = "Removed \"KevCoorp\" in Contact Me";
+        document.getElementById("version").innerHTML = "Removed the version of the website in About me";
+        document.getElementById("navigation").innerHTML = "Changed the style of the navigation bar";
+        document.getElementById("background").innerHTML = "The changelog's background is now grey.";
+        document.getElementById("navigation2").innerHTML = "Changed the navigation bar's colour to translucent green.";
+        document.getElementById("myself").innerHTML = "The presentation of myself has been changed.";
+        document.getElementById("kevcoorp2").innerHTML = "\"KevCoorp\" is now placed below the site build.";
+        document.getElementById("99").innerHTML = "Build 99 Translation Update";
+        document.getElementById("english").innerHTML = "English translation of the site";
+        document.getElementById("design").innerHTML = "Some design changes";
+        document.getElementById("comingsoon").innerHTML = "More updates are planned in the future.";
+    } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
+        document.getElementById("correction").innerHTML = "Corrections d'un problème de fond-d'écran lorsqu'on dé-zoome";
+        document.getElementById("javascript").innerHTML = "Test de Javascript (Dans le code uniquement)";
+        document.getElementById("changelog").innerHTML = "Ajout d'une catégorie \"Changelog\"";
+        document.getElementById("kevcoorp").innerHTML = "Suppresion de \"KevCoorp\" dans la section \"Me contactez\"";
+        document.getElementById("version").innerHTML = "Suppresion de la version du site dans la section \"A propos\"";
+        document.getElementById("navigation").innerHTML = "Changement du style de la barre de navigation.";
+        document.getElementById("background").innerHTML = "Le Background utilisé dans le changelog a était remplaçé par un fond gris.";
+        document.getElementById("navigation2").innerHTML = "La Barre de navigation change encore une fois de couleur pour passer au vert transparent.";
+        document.getElementById("myself").innerHTML = "La présentation de moi-meme à était changé.";
+        document.getElementById("kevcoorp2").innerHTML = "L'emplacement de \"KevCoorp\" ce trouve en dessous des builds du site.";
+        document.getElementById("99").innerHTML = "Build 99 Traduction Update";
+        document.getElementById("english").innerHTML = "Traduction en anglais du site";
+        document.getElementById("design").innerHTML = "Quelques changements de conception";
+        document.getElementById("comingsoon").innerHTML = "D'autres Builds sont prévus prochainnement.";
+    }
+    }
+    function langSwitch() {
+        var langId = document.getElementById("language").value;
+        document.cookie = `lang=${langId};`
+        if (document.cookie.split(';').some((item) => item.includes('lang=en'))) {
+            document.getElementById("correction").innerHTML = "Fixed a problem with the background when zooming out!";
+            document.getElementById("javascript").innerHTML = "Javascript test";
+            document.getElementById("changelog").innerHTML = "Added the changelog";
+            document.getElementById("kevcoorp").innerHTML = "Removed \"KevCoorp\" in Contact Me";
+            document.getElementById("version").innerHTML = "Removed the version of the website in About me";
+            document.getElementById("navigation").innerHTML = "Changed the style of the navigation bar";
+            document.getElementById("background").innerHTML = "The changelog's background is now grey.";
+            document.getElementById("navigation2").innerHTML = "Changed the navigation bar's colour to translucent green.";
+            document.getElementById("myself").innerHTML = "The presentation of myself has been changed.";
+            document.getElementById("kevcoorp2").innerHTML = "\"KevCoorp\" is now placed below the site build.";
+            document.getElementById("99").innerHTML = "Build 99 Translation Update";
+            document.getElementById("english").innerHTML = "English translation of the site";
+            document.getElementById("design").innerHTML = "Some design changes";
+            document.getElementById("comingsoon").innerHTML = "More updates are planned in the future.";
+        } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
+            document.getElementById("correction").innerHTML = "Corrections d'un problème de fond-d'écran lorsqu'on dé-zoome";
+            document.getElementById("javascript").innerHTML = "Test de Javascript (Dans le code uniquement)";
+            document.getElementById("changelog").innerHTML = "Ajout d'une catégorie \"Changelog\"";
+            document.getElementById("kevcoorp").innerHTML = "Suppresion de \"KevCoorp\" dans la section \"Me contactez\"";
+            document.getElementById("version").innerHTML = "Suppresion de la version du site dans la section \"A propos\"";
+            document.getElementById("navigation").innerHTML = "Changement du style de la barre de navigation.";
+            document.getElementById("background").innerHTML = "Le Background utilisé dans le changelog a était remplaçé par un fond gris.";
+            document.getElementById("navigation2").innerHTML = "La Barre de navigation change encore une fois de couleur pour passer au vert transparent.";
+            document.getElementById("myself").innerHTML = "La présentation de moi-meme à était changé.";
+            document.getElementById("kevcoorp2").innerHTML = "L'emplacement de \"KevCoorp\" ce trouve en dessous des builds du site.";
+            document.getElementById("99").innerHTML = "Build 99 Traduction Update";
+            document.getElementById("english").innerHTML = "Traduction en anglais du site";
+            document.getElementById("design").innerHTML = "Quelques changements de conception";
+            document.getElementById("comingsoon").innerHTML = "D'autres Builds sont prévus prochainnement.";
+        }
+    }
