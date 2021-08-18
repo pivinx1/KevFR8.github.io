89bccdc6e3f94096073b202d50bdbc6099e42aec
zai-tm <tusharmistry444@gmail.com>
2021-08-18 17:37:26 -0600

    OVERHAUL

--- lang/language2.js
+++ lang/language2.js
@@ -5,10 +5,12 @@ if (document.cookie.split(';').some((item) => item.includes('lang=en'))) {
     document.getElementById("title").innerHTML = "About me";
     document.getElementById("who").innerHTML = "Who am I?";
     document.getElementById("descr").innerHTML = "Hey, I'm KevFR! I'm a fan of computers and video games. I make videos about the game Progressbar 95 and tutorials on how to install certain OSs. Don't hesitate to contact me about this!";
+    document.getElementById("click").innerHTML = "<i>psst, i'm clickable!</i>"
 } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
     document.getElementById("title").innerHTML = "A propos de moi";
     document.getElementById("who").innerHTML = "Qui je suis ?";
     document.getElementById("descr").innerHTML = "Hey je suis KevFR ! Je suis un passoné d'informatique et de jeux-vidéos ! Je fais des vidéos sur le jeu Progressbar95 et des tutoriels sur comment installer un OS en particulier ou simplement une simple vidéo ! N'hésite pas à me regoindresur ce je te laisse !  ";
+    document.getElementById("click").innerHTML = "<i>psst, je suis cliquable !</i>"
 }
 }
 function langSwitch() {
@@ -18,9 +20,11 @@ function langSwitch() {
         document.getElementById("title").innerHTML = "About me";
         document.getElementById("who").innerHTML = "Who am I?";
         document.getElementById("descr").innerHTML = "Hey, I'm KevFR! I'm a fan of computers and video games. I make videos about the game Progressbar 95 and tutorials on how to install certain OSs. Don't hesitate to contact me about this!";
+        document.getElementById("click").innerHTML = "<i>psst, i'm clickable!</i>"
     } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
         document.getElementById("title").innerHTML = "A propos de moi";
         document.getElementById("who").innerHTML = "Qui je suis ?";
         document.getElementById("descr").innerHTML = "Hey je suis KevFR ! Je suis un passoné d'informatique et de jeux-vidéos ! Je fais des vidéos sur le jeu Progressbar95 et des tutoriels sur comment installer un OS en particulier ou simplement une simple vidéo ! N'hésite pas à me regoindresur ce je te laisse !  ";
+        document.getElementById("click").innerHTML = "<i>psst, je suis cliquable !</i>"
     }
 }
