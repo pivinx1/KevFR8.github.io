59df136480d94baa3d95803000cc5c7c7f016792
zai-tm <tusharmistry444@gmail.com>
2021-08-18 15:31:11 -0600

    stuff

---
+++ time.js
@@ -0,0 +1,18 @@
+function Time() {
+    var date = new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    hour = update(hour);
+    minute = update(minute);
+    document.getElementById("digital-clock").innerText = hour + ":" + minute;
+    setTimeout(Time, 1000);
+   }
+   function update(t) {
+    if (t < 10) {
+    return "0" + t;
+    }
+    else {
+    return t;
+    }
+   }
+   Time();
