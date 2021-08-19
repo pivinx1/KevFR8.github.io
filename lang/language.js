88c38370b0661e6266ef80b668a30e334b7bfc5a
zai-tm <tusharmistry444@gmail.com>
2021-08-18 21:31:10 -0600

    taskbar on all pages

--- lang/language.js
+++ lang/language.js
@@ -3,22 +3,24 @@ function cookieLang() {
     if (document.cookie.split(';').some((item) => item.includes('lang=en'))) {
         document.getElementById("language").value = "en";
         document.getElementById("welcome").innerHTML = "Welcome to FireWave!";
-        document.getElementById("windows7pack").innerHTML = "Windows 7 pack";
-        document.getElementById("about").innerHTML = "About me";
-        document.getElementById("contact").innerHTML = "Contact me";
         document.getElementById("computer_fan").innerHTML = "Are you a fan of computers? This site lets you download my packs and much more!<br/> Don't hesitate to contact me in case of problems!";
         document.getElementById("current_video").innerHTML = "Latest video";
         document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Translations done by Zai / HTML/CSS help by Luihum";
+        document.getElementById("windows7pack").innerHTML = "Windows 7 pack";
+        document.getElementById("about").innerHTML = "About me";
+        document.getElementById("contact").innerHTML = "Contact me";
         document.getElementById("help").innerHTML = "Help develop the site!";
+        document.getElementById("shut").innerHTML = "Shut Down";
     } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
         document.getElementById("welcome").innerHTML = "Bienvenue sur FireWave !";
-        document.getElementById("windows7pack").innerHTML = "Pack Windows 7";
-        document.getElementById("about").innerHTML = "A propois de moi";
-        document.getElementById("contact").innerHTML = "Me Contactez";
         document.getElementById("computer_fan").innerHTML = "Fan de l'informatique ? Ce site vous permet de découvrir mes packs et bien plus encore !<br/>N'hésitez pas à me contactez en cas de problèmes !";
         document.getElementById("current_video").innerHTML = "La vidéo du moment !";
         document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Aide & Traduction en Anglais par Zai / Aide HTML/CSS Luihum";
+        document.getElementById("windows7pack").innerHTML = "Pack Windows 7";
+        document.getElementById("about").innerHTML = "A propois de moi";
+        document.getElementById("contact").innerHTML = "Me Contactez";
         document.getElementById("help").innerHTML = "Aidez à développer ce site !";
+        document.getElementById("shut").innerHTML = "Fermer";
     }
     }
     function langSwitch() {
@@ -26,21 +28,23 @@ function cookieLang() {
         document.cookie = `lang=${langId};`
         if (document.cookie.split(';').some((item) => item.includes('lang=en'))) {
             document.getElementById("welcome").innerHTML = "Welcome to FireWave!";
-            document.getElementById("windows7pack").innerHTML = "Windows 7 pack";
-            document.getElementById("about").innerHTML = "About me";
-            document.getElementById("contact").innerHTML = "Contact me";
             document.getElementById("computer_fan").innerHTML = "Are you a fan of computers? This site lets you download my packs and much more!<br/> Don't hesitate to contact me in case of problems!";
             document.getElementById("current_video").innerHTML = "Latest video";
             document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Translations done by Zai / HTML/CSS help by Luihum";
+            document.getElementById("windows7pack").innerHTML = "Windows 7 pack";
+            document.getElementById("about").innerHTML = "About me";
+            document.getElementById("contact").innerHTML = "Contact me";
             document.getElementById("help").innerHTML = "Help develop the site!";
+            document.getElementById("shut").innerHTML = "Shut Down";
         } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
             document.getElementById("welcome").innerHTML = "Bienvenue sur FireWave !";
-            document.getElementById("windows7pack").innerHTML = "Pack Windows 7";
-            document.getElementById("about").innerHTML = "A propois de moi";
-            document.getElementById("contact").innerHTML = "Me Contactez";
             document.getElementById("computer_fan").innerHTML = "Fan de l'informatique ? Ce site vous permet de découvrir mes packs et bien plus encore !<br/>N'hésitez pas à me contactez en cas de problèmes !";
             document.getElementById("current_video").innerHTML = "La vidéo du moment !";
             document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Aide & Traduction en Anglais par Zai / Aide HTML/CSS Luihum";
+            document.getElementById("windows7pack").innerHTML = "Pack Windows 7";
+            document.getElementById("about").innerHTML = "A propois de moi";
+            document.getElementById("contact").innerHTML = "Me Contactez";
             document.getElementById("help").innerHTML = "Aidez à développer ce site !";
+            document.getElementById("shut").innerHTML = "Fermer";
         }
     }
--- lang/language2.js
+++ lang/language2.js
@@ -6,11 +6,21 @@ if (document.cookie.split(';').some((item) => item.includes('lang=en'))) {
     document.getElementById("who").innerHTML = "Who am I?";
     document.getElementById("descr").innerHTML = "Hey, I'm KevFR! I'm a fan of computers and video games. I make videos about the game Progressbar 95 and tutorials on how to install certain OSs. Don't hesitate to contact me about this!";
     document.getElementById("click").innerHTML = "<i>psst, i'm clickable!</i>"
+    document.getElementById("windows7pack").innerHTML = "Windows 7 pack";
+    document.getElementById("about").innerHTML = "About me";
+    document.getElementById("contact").innerHTML = "Contact me";
+    document.getElementById("help").innerHTML = "Help develop the site!";
+    document.getElementById("shut").innerHTML = "Shut Down";
 } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
     document.getElementById("title").innerHTML = "A propos de moi";
     document.getElementById("who").innerHTML = "Qui je suis ?";
     document.getElementById("descr").innerHTML = "Hey je suis KevFR ! Je suis un passoné d'informatique et de jeux-vidéos ! Je fais des vidéos sur le jeu Progressbar95 et des tutoriels sur comment installer un OS en particulier ou simplement une simple vidéo ! N'hésite pas à me regoindresur ce je te laisse !  ";
     document.getElementById("click").innerHTML = "<i>psst, je suis cliquable !</i>"
+    document.getElementById("windows7pack").innerHTML = "Pack Windows 7";
+    document.getElementById("about").innerHTML = "A propois de moi";
+    document.getElementById("contact").innerHTML = "Me Contactez";
+    document.getElementById("help").innerHTML = "Aidez à développer ce site !";
+    document.getElementById("shut").innerHTML = "Fermer";
 }
 }
 function langSwitch() {
@@ -21,10 +31,20 @@ function langSwitch() {
         document.getElementById("who").innerHTML = "Who am I?";
         document.getElementById("descr").innerHTML = "Hey, I'm KevFR! I'm a fan of computers and video games. I make videos about the game Progressbar 95 and tutorials on how to install certain OSs. Don't hesitate to contact me about this!";
         document.getElementById("click").innerHTML = "<i>psst, i'm clickable!</i>"
+        document.getElementById("windows7pack").innerHTML = "Windows 7 pack";
+        document.getElementById("about").innerHTML = "About me";
+        document.getElementById("contact").innerHTML = "Contact me";
+        document.getElementById("help").innerHTML = "Help develop the site!";
+        document.getElementById("shut").innerHTML = "Shut Down";
     } else if (document.cookie.split(';').some((item) => item.includes('lang=fr'))) {
         document.getElementById("title").innerHTML = "A propos de moi";
         document.getElementById("who").innerHTML = "Qui je suis ?";
         document.getElementById("descr").innerHTML = "Hey je suis KevFR ! Je suis un passoné d'informatique et de jeux-vidéos ! Je fais des vidéos sur le jeu Progressbar95 et des tutoriels sur comment installer un OS en particulier ou simplement une simple vidéo ! N'hésite pas à me regoindresur ce je te laisse !  ";
         document.getElementById("click").innerHTML = "<i>psst, je suis cliquable !</i>"
+        document.getElementById("windows7pack").innerHTML = "Pack Windows 7";
+        document.getElementById("about").innerHTML = "A propois de moi";
+        document.getElementById("contact").innerHTML = "Me Contactez";
+        document.getElementById("help").innerHTML = "Aidez à développer ce site !";
+        document.getElementById("shut").innerHTML = "Fermer";
     }
 }
