8de385be14f16afa2241f0b514befc615d99851f
KevFR <70813133+KevFR8@users.noreply.github.com>
2021-08-19 20:24:20 +0200

    Remove
    La spéfication "KevCoorp" a était enlevée

--- lang/language.js
+++ lang/language.js
@@ -5,7 +5,6 @@ function cookieLang() {
         document.getElementById("welcome").innerHTML = "Welcome to FireWave!";
         document.getElementById("computer_fan").innerHTML = "Are you a fan of computers? This site lets you download my packs and much more!<br/> Don't hesitate to contact me in case of problems!";
         document.getElementById("current_video").innerHTML = "Latest video";
-        document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Translations done by Zai / HTML/CSS help by Luihum";
         document.getElementById("windows7pack").innerHTML = "Windows 7 pack";
         document.getElementById("about").innerHTML = "About me";
         document.getElementById("contact").innerHTML = "Contact me";
@@ -15,7 +14,6 @@ function cookieLang() {
         document.getElementById("welcome").innerHTML = "Bienvenue sur FireWave !";
         document.getElementById("computer_fan").innerHTML = "Fan de l'informatique ? Ce site vous permet de découvrir mes packs et bien plus encore !<br/>N'hésitez pas à me contactez en cas de problèmes !";
         document.getElementById("current_video").innerHTML = "La vidéo du moment !";
-        document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Aide & Traduction en Anglais par Zai / Aide HTML/CSS Luihum";
         document.getElementById("windows7pack").innerHTML = "Pack Windows 7";
         document.getElementById("about").innerHTML = "A propos de moi";
         document.getElementById("contact").innerHTML = "Me Contactez";
@@ -30,7 +28,6 @@ function cookieLang() {
             document.getElementById("welcome").innerHTML = "Welcome to FireWave!";
             document.getElementById("computer_fan").innerHTML = "Are you a fan of computers? This site lets you download my packs and much more!<br/> Don't hesitate to contact me in case of problems!";
             document.getElementById("current_video").innerHTML = "Latest video";
-            document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Translations done by Zai / HTML/CSS help by Luihum";
             document.getElementById("windows7pack").innerHTML = "Windows 7 pack";
             document.getElementById("about").innerHTML = "About me";
             document.getElementById("contact").innerHTML = "Contact me";
@@ -40,7 +37,6 @@ function cookieLang() {
             document.getElementById("welcome").innerHTML = "Bienvenue sur FireWave !";
             document.getElementById("computer_fan").innerHTML = "Fan de l'informatique ? Ce site vous permet de découvrir mes packs et bien plus encore !<br/>N'hésitez pas à me contactez en cas de problèmes !";
             document.getElementById("current_video").innerHTML = "La vidéo du moment !";
-            document.getElementById("copyright").innerHTML = "&copy;KevCoorp's 2017 - 2021<br/>Aide & Traduction en Anglais par Zai / Aide HTML/CSS Luihum";
             document.getElementById("windows7pack").innerHTML = "Pack Windows 7";
             document.getElementById("about").innerHTML = "A propos de moi";
             document.getElementById("contact").innerHTML = "Me Contactez";
